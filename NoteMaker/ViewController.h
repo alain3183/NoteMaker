cc0cc749f6e7e42cbc7c55c77485e0b1bd454a32
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-09 12:33:28 -0500

    Initial Commit

---
+++ NoteMaker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NoteMaker
+//
+//  Created by Alain Malenfant on 11/9/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NoteMaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NoteMaker
+//
+//  Created by Alain Malenfant on 11/9/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
